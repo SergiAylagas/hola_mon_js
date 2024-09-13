da9816f937eaece028b6665d0059402179b1aef7
Sergi Aylagas <sergi.aylagas@cirvianum.cat>
2024-09-13 19:10:53 +0200

    Primera pujada del projecte Hola Món amb JS

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function saluda() {
+    console.log("Hola món! Has clicat el botó!");
+  }
+
+/* AMB AQUEST CODI FEM QUE ES MOSTRI EL MISSATGE A LA PAGINA WEB
+function saluda() {
+  document.getElementById("message").innerHTML = "Hola món! Has clicat el botó!";
+}
+*/
